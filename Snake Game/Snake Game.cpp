4d09b3844ab10f2be018b6834dfef90c9dee105e
EliYakubov7 <yeliyahu7@gmail.com>
2020-05-14 14:30:03 +0300

    CREATE README.md

--- Snake Game/Snake Game.cpp
+++ Snake Game/Snake Game.cpp
@@ -3,6 +3,7 @@
 #include <windows.h>
 using namespace std;
 
+
 bool GameOver;
 const int width = 60;
 const int height = 30;
@@ -201,10 +202,19 @@ void Logic()
 
 }
 
+void fullscreen()
+{
+	keybd_event(VK_MENU, 0x38, 0, 0);
+	keybd_event(VK_RETURN, 0x1c, 0, 0);
+	keybd_event(VK_RETURN, 0x1c, KEYEVENTF_KEYUP, 0);
+	keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);
+}
+
 
 int main()
 {
 	Setup();
+	fullscreen();
 
 	while (!GameOver)
 	{
